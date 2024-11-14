c6be44509b6d848fb395378a663a0a02f04a35e2
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2024-11-14 09:00:41 -0500

    Add latest blog post

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,24 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
-        <p className="text-4xl font-bold">Jorge Padilla</p>
-        <p className="mb-2">Software Engineer</p>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-2">Jorge Padilla</h1>
+          <p className="mb-2">Software Engineer</p>
+        </div>
+
+        <h2 className="text-xl font-bold">Latest blog post</h2>
+        <ul>
+          <li className="flex items-center gap-2">
+            <a
+              className="hover:underline hover:underline-offset-4"
+              href="https://medium.com/kubeshop-i/tracetest-monitors-synthetic-monitoring-with-opentelemetry-and-playwright-6dba002b8377"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Synthetic Monitoring with OpenTelemetry and Playwright
+            </a>
+          </li>
+        </ul>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
